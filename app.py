3a9a3879e36c78c0aa58bb5dd829132e240cdc2c
cheeksai <jacksonrmeans@gmail.com>
2025-09-15 09:05:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -423,14 +423,13 @@ def simulate_game(team1, team2):
     goals2 = sum(1 for goal in all_goals if goal[3] == team1)
     saves2 = shots - goals
 
+    goals2 = sum(1 for goal in all_goals if goal[3] == team2)
+    
     shots1 = saves2
     saves1 = shots1 - goals2
     
     shots2_list.append(shots2)
     
-        
-    goals2 = sum(1 for goal in all_goals if goal[3] == team2)
-
     
 
     player_goal_sequence = {}
